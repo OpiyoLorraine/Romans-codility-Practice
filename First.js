86c106fa60916d6d423dcfe67a5eaa5c1a725725
Davincii254 <victorkuria7@gmail.com>
2024-07-29 09:08:35 +0300

    Add question

--- First.js
+++ First.js
@@ -1,43 +1,24 @@
-// You are given a list of N transfers (numbered from 0 to N−1) between two banks: bank A and bank B. The K-th transfer is described by two values:
+// An industrial company has N factories, each producing some pollution every month. The company has decided to reduce its total fume emissions by equipping some of the factories with one or more filters.
 
-// R[K] (either “A” or “B”) representing the recipient (the bank the transfer is sent to);
-// V[K] denoting the value sent via the transfer.
-// All transfers are completed in the order they appear on the list. The banks do not want to go into debt (in other words, their account balance may not drop below 0). What minimum initial account balance in each bank is necessary in order to complete the transfers?
+// Every such filter reduces the pollution of a factory by half. When a second (or subsequent) filter is applied, it again reduces by half the remaining pollution emitted after fitting the existing filter(s). For example, a factory that initially produces 6 units of pollution will generate 3 units with one filter, 1.5 units with two filters and 0.75 units with three filters.
 
-// Assume that the following declarations are given:
-
-// struct Results {
-// int * A;
-// int M; // Length of the array
-// };
+// You are given an array of N integers describing the amount of pollution produced by the factories. Your task is to find the minimum number of filters needed to decrease the total sum of pollution by at least half.
 
 // Write a function:
 
-// struct Results solution(char *R, int V[], int N);
+// int solution(int A[], int N);
 
-// that, given a string R and an array of integers V, both of length N, returns an array of two integers. The integers should represent the minimum initial account balances for banks A and B in the following order: [bank A, bank B].
-
-// Result array should be returned as a structure Results.
+// which, given an array of integers A of length N, returns the minimum number of filters needed to reduce the total pollution by at least half.
 
 // Examples:
 
-// Given R = “BAABA” and V = [2, 4, 1, 1, 2], the function should return [2, 4]. The bank accounts’ balances after each transfer are shown in the following table:
-//                        | A | B
-// ------------------------±–±--
-// initial balance | 2 | 4
-// transfer 2 from A to B | 0 | 6
-// transfer 4 from B to A | 4 | 2
-// transfer 1 from B to A | 5 | 1
-// transfer 1 from A to B | 4 | 2
-// transfer 2 from B to A | 6 | 0
+// 1. Given A = [5, 19, 8, 1], your function should return 3. The initial total pollution is 5 + 19 + 8 + 1 = 33. We install two filters at the factory producing 19 units and one filter at the factory producing 8 units. Then the total pollution will be 5 + ((19 / 2) / 2) + (8 / 2) + 1 = 5 + 4.75 + 4 + 1 = 14.75 which is less than 33 / 2 = 16.5, so we have achieved our goal.
 
-// Given R = “ABAB” and V = [10, 5, 10, 15], the function should return [0, 15].
+// 2. Given A = [10, 10], your function should return 2, because we may install one filter at each factory.
 
-// Given R = “B” and V = [100], the function should return [100, 0].
+// 3. Given A = [3, 0, 5], your function should return 2, because it is sufficient to install one filter at each factory producing a non-zero amount of pollution.
 
 // Write an efficient algorithm for the following assumptions:
 
-// string R and array V are both of length N;
-// N is an integer within the range [1…100,000];
-// each element of array V is an integer within the range [1…10,000];
-// string R is made only of the characters ‘A’ and/or ‘B’.
+// N is an integer within the range [1..30,000];
+// each element of array A is an integer within the range [0..70,000].
